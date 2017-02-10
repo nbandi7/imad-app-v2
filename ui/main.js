b66733102f11cd8b3ed5fb1ffafa65546a4ac2ae
Nithin C Bandi <nithincbandi@gmail.com>
2017-02-10 18:51:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,10 +29,10 @@ var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
     //make a request to the server and send the name
-  
+    
   
     //capture the list and render it as a list
-    var names = ['name1','name2','name3','name4'];
+    var names = req.query.name;
     var list = '';
     for(var i=0;i< names.length;i++){
         list+= '<li>' + names[i] + '</li>';
