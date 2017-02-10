ba4d51ffe784561db6494bd7df02362364e60a62
Nithin C Bandi <nithincbandi@gmail.com>
2017-02-10 16:52:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,25 @@
 //counter code
 var button = document.getElementById("counter");
-var counter = 0;
+
 button.onclick = function(){
     
+    //cretae a request object
+    var counter = new XMLHttpRequest();
     
-    //render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
-}
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+            
+        }
+    };
+    //Make a request
+    request.open('GET', 'http://nbandi7.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
