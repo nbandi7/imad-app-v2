ab59ea84190704507a90d3484ee77c341bf5f234
Nithin C Bandi <nithincbandi@gmail.com>
2017-02-10 18:47:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/submit-name:name',function(req,res){
    names.push(name);
    //JSON
    
-   res.send(JSON.stringfy(names)); //TODO
+   res.send(JSON.stringify(names)); //TODO
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
