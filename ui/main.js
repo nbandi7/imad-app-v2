3cd5746b3527451b453f83298cef0429d516189b
Nithin C Bandi <nithincbandi@gmail.com>
2017-02-10 18:27:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,10 +34,10 @@ submit.onclick = function(){
     //capture the list and render it as a list
     var names = ['name1','name2','name3','name4'];
     var list = '';
-    for(var i=0;i< name.length;i++){
+    for(var i=0;i< names.length;i++){
         list+= '<li>' + names[i] + '</li>';
     }
-    var ul =document.getElementById('namelist');
+    var ul =document.getElementById("namelist");
     ul.innerHTML = list;
     
 };
