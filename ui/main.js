a599d20932d7892ca2dd4cd62e1617cb6d414e15
Nithin C Bandi <nithincbandi@gmail.com>
2017-02-10 15:26:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ console.log('Loaded!');
 //move the image
 var img = document.getElementById("madi");
 img.onclick = function(){
-    var interval = setInterval(moveLeft,100)
+    var interval = setInterval(moveLeft,100);
     img.style.moveLeft = '100px';
 };
