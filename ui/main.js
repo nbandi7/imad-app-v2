ce9098698fc7ccb2ba727748909acdb5c2ef1c3a
Nithin C Bandi <nithincbandi@gmail.com>
2017-02-10 15:29:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ console.log('Loaded!');
 
 //move the image
 var img = document.getElementById("madi");
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft * 10;
     img.style.marginLeft = marginLeft + 'px';
