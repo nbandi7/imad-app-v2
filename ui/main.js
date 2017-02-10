f0bec1d8cbe2a5538837eb97341b51d4d37d6bbe
Nithin C Bandi <nithincbandi@gmail.com>
2017-02-10 18:25:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,11 +15,29 @@ button.onclick = function() {
                 var span = document.getElementById("count");
                 span.innerHTML = counter.toString();
             }
-            
         }
     };
     //Make a request
     request.open('GET', 'http://nbandi7.imad.hasura-app.io/counter',true);
     request.send(null);
+    
+};
+
+//submit name
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function(){
+    //make a request to the server and send the name
+  
+  
+    //capture the list and render it as a list
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0;i< name.length;i++){
+        list+= '<li>' + names[i] + '</li>';
+    }
+    var ul =document.getElementById('namelist');
+    ul.innerHTML = list;
     
 };
