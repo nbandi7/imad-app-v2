d0b9b706429e305a16bc490ac05529cc35700345
Nithin C Bandi <nithincbandi@gmail.com>
2017-03-02 00:41:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 });
 
 
-var Pool = new Pool(config);
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query("SELECT * FROM test",function(err,result){
         if(err){
