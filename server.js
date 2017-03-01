9fed5157e53b2b51407b7ccb76d534d75cd9b4b2
Nithin C Bandi <nithincbandi@gmail.com>
2017-03-02 00:38:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user : 'nbandi7',
+    database : 'nbandi7',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+    
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -13,6 +23,22 @@ app.get('/counter', function (req, res) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+var Pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query("SELECT * FROM test",function(err,result){
+        if(err){
+            res.status(500).send(err,toString());
+        }else{
+            res.send(JSON.stringfy(result));
+        }
+        
+    });
+    
+    
+    
 });
 
 app.get('/ui/style.css', function (req, res) {
