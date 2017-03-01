9181a26a81785b0cb7937d20d3a5ef8dc6bda3f0
Nithin C Bandi <nithincbandi@gmail.com>
2017-03-02 02:32:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/articles/:articleName', function (req, res) {
       if(err){
           res.status(500).send(err.toString());
       } else {
-          if(results.rows.length ===0) {
+          if(result.rows.length ===0) {
             res.status(404).send('Article not found');
           } else {
               var articleData = result.rows[0];
