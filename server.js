133ece435ea39bd6a058fcc7658191e1ceaeacb7
Nithin C Bandi <nithincbandi@gmail.com>
2017-03-04 01:16:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,10 +86,8 @@ app.get('/articles/:articleName', function (req, res) {
               var articleData = result.rows[0];
               res.send(createTemplate(articleData));
           }
-          
       }
   });
-  
 });
 
 app.get('/ui/style.css', function (req, res) {
