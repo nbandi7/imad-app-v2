148da81e6f386f8200825170103c5ebb30c64470
Nithin C Bandi <nithincbandi@gmail.com>
2017-03-04 01:54:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user : 'nbandi7',
@@ -60,6 +61,17 @@ function createTemplate(Data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Sync('secret', 'salt', 100000, 512, 'sha512');
+    return hashed.toString('hex');
+
+}
+
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input , 'this-is-some-random-string');
+    res.send(hashedString) ;
 });
 
 
